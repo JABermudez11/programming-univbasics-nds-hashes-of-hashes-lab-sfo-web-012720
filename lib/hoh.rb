fbfa225a4416d36343acda68d3ac8b840870ded0
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-17 13:26:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,9 @@ def naming_system
       label: "Phylum",
       sub_category: {
         label: "Class",
-        sub_category: "Order"
+        sub_category: {
+          "Order"
+        }
       }
     }
   }
